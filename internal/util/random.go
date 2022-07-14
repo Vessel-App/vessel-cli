6a072895e083610b3b8f8031c2d428b6cf1ba8b4
Chris Fidao <fideloper@gmail.com>
2022-07-13 21:37:58 -0500

    random helpers

---
+++ internal/util/random.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandomString(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
